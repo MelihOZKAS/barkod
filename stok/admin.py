c3a2479cf2a6c52149795115d3d3ec7b25d6c629
MelihOZKAS <melihozkass@gmail.com>
2024-02-01 12:43:44 +0300

    Nasip 73

--- stok/admin.py
+++ stok/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django import forms
 from django.db import models
 import math
+from decimal import Decimal
+
 
 
 
@@ -38,13 +40,13 @@ class StokAdmin(admin.ModelAdmin):
 
     def Yuzde10ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.10) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.10")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde10ZamYap.short_description = "Seçili ürünlere Yuzde:10 zam uygula"
     def Yuzde15ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.15) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.15")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde15ZamYap.short_description = "Seçili ürünlere Yuzde:15 zam uygula"
@@ -57,27 +59,27 @@ class StokAdmin(admin.ModelAdmin):
 
     def Yuzde20ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.20) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.20")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde20ZamYap.short_description = "Seçili ürünlere Yuzde:20 zam uygula"
     def Yuzde25ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.25) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.25")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde25ZamYap.short_description = "Seçili ürünlere Yuzde:25 zam uygula"
 
     def Yuzde30ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.30) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.30")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde30ZamYap.short_description = "Seçili ürünlere Yuzde:30 zam uygula"
 
     def Yuzde35ZamYap(self, request, queryset):
         for obj in queryset:
-            obj.Tutar = math.floor(obj.Tutar * 1.35) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
+            obj.Tutar = math.floor(obj.Tutar * Decimal("1.35")) + 1  # %25 zam uygula, küsüratı sil ve 1 ekle
             obj.save()
 
     Yuzde30ZamYap.short_description = "Seçili ürünlere Yuzde:35 zam uygula"
